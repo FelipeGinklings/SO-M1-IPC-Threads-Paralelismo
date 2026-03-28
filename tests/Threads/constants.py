3cc25c73538a2708427df8e84cf4f4a57d55cf73
Felipe Ginklings <felipe.ginklings@gmail.com>
2026-03-28 18:48:34 -0300

    Add multithreading support for image processing in new notebook
    - Implemented functions to calculate optimal thread count based on image height.
    - Added a section to sum parts of a vector using threads with locking for shared results.
    - Created a test suite for various image sizes to demonstrate threading efficiency.
    - Included example usage of threading for summing a vector.

---
+++ tests/Threads/constants.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class PGM:
+    height: int
+    width: int
+    maxv: int
+    data: bytes
+
+    def to_dict(self):
+        return {
+            "width": self.width,
+            "height": self.height,
+            "maxv": self.maxv,
+        }
+
+    def __str__(self):
+        return f"height: {self.height}, width: {self.width}, maxv: {self.maxv}"
+
+
+PIPE_PATH = "/tmp/my_pipe"
